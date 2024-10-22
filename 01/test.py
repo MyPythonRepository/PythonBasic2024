3aa017e1c708b5e47a809ab89998162d7ccf2637
MyPythonRepository <iryna.sharova@ukr.net>
2024-10-22 17:47:15 +0300

    Second commit

---
+++ 01/test.py
@@ -0,0 +1,7 @@
+# from langdetect import detect
+# print(detect('Today is a gret day'))
+# print(detect("Guten Morgen!"))
+# print(detect("Вітаю!"))
+
+print('Hello World!')
+print('Hello People!')
---
+++ 01/test2.py
@@ -0,0 +1,8 @@
+# import math Ctrl +  /
+'''import math'''
+
+from math import pi
+
+print('Hello')
+# print(math.pi)
+print(pi)
